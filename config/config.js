7f0e96cc45117f6d5c5ef61077e09e12967175c4
Vivek Malhan <vivek.malhan@yahoo.com>
2018-11-05 19:43:09 -0500

    testing

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-  db: process.env.mongolab-symmetrical-10527,
+  db: process.env.MONGODB_URI,
   db_dev: 'mongodb://localhost:27017/rateMyProject',
 };
